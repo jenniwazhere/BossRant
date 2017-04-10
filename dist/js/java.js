40fe54a72b420a5bc389fe6ad6a00316bb332caf
Nhat Nguyen <nhatrockynguyen@gmail.com>
2017-04-10 16:01:29 -0700

    File organization and small updates.

--- dist/js/java.js
+++ dist/js/java.js
@@ -1,13 +1,11 @@
-function incrementValueRough()
-{
+function incrementValueRough() {
     var value = parseInt(document.getElementById('numberA').value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
     document.getElementById('numberA').value = value;
 }
 
-function incrementValueDisserve()
-{
+function incrementValueDisserve() {
     var value = parseInt(document.getElementById('numberD').value, 10);
     value = isNaN(value) ? 0 : value;
     value++;
